f4dbac028a6affc80f596c9e947838646ce9ba71
Kiran Duggirala <kduggirala10@stuy.edu>
2019-03-11 21:52:32 -0400

    Tested quickselect with slightly different numbers: slightly glichy in == condition

--- Quick.java
+++ Quick.java
@@ -1,11 +1,12 @@
 
 public class Quick{
 	public static void main(String[] args) {
-		int[] data = {17, 61, 67, 47, 93,12, 20, 4, 44, 68, 1234, 13, 34, 57, 16, 85, 21, 134, 136, 137326};
+		int[] data = {17, 61, 67, 47, 93,12, 20, 4, 44, 68, 1234, 13, 13, 34, 57, 16, 85, 21, 134, 136, 137326};
 		for (int i = 0; i < data.length; i++) {
 			System.out.println(quickselect(data, i));
 		}
 	}
+	
 	public static int quickselect(int[] data, int k) {
 		int start = 0;
 		int end = data.length - 1;
